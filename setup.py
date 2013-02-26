6c2ec848d4403ffac7b464f61c93e87fa71c5228
Enrico Giampieri <enrico.giampieri@unibo.it>
2013-02-26 17:45:58 +0100

    creation of the setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='keggrest',
+      version='0.1',
+      description='Basic frontend for the kegg database rest API',
+      author='Enrico Giampieri',
+      author_email='enrico.giampieri@unibo.it',
+      py_modules=['keggrest'],
+      )
