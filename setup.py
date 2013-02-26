8b6e963530fcce4d3f132ef61677684176c2e9d2
Enrico Giampieri <enrico.giampieri@unibo.it>
2013-02-26 17:50:40 +0100

    added the github address to the setup file

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ setup(name='keggrest',
       author='Enrico Giampieri',
       author_email='enrico.giampieri@unibo.it',
       py_modules=['keggrest'],
+      download_url='https://github.com/EnricoGiampieri/keggrest.git'
       )
