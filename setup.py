526fbee041f98c54f8e80fa2a560cf947d58f8ee
Enrico Giampieri <enrico.giampieri@unibo.it>
2013-03-06 17:34:12 +0100

    update the setup script

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+import os
+from setuptools import setup
+
+# Utility function to read the README file.  
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='keggrest',
       version='0.1',
@@ -9,4 +17,12 @@ setup(name='keggrest',
       py_modules=['keggrest'],
       download_url='https://github.com/EnricoGiampieri/keggrest.git',
       url='https://github.com/EnricoGiampieri/keggrest.git',
+      license = "BSD",
+      long_description=read('README.txt'),
+      keywords = "rest API bioinformatics",
+      classifiers=[
+          "Development Status :: 0.1",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: BSD License",
+          ],
       )
