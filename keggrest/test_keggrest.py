2ef25288834aa8bed02fa6c5cfaca4f6f8ca7211
EnricoGiampieri <enrico.giampieri@unibo.it>
2014-04-14 11:56:32 +0200

    created test file for future testing

---
+++ keggrest/test_keggrest.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 14 11:52:46 2014
+
+@author: enrico.giampieri2
+"""
+
+import keggrest
+import unittest
+
+class SimplisticTest(unittest.TestCase):
+    def test(self):
+        self.assertTrue(True)
+        
+if __name__ == '__main__':
+    unittest.main()
